c50127ef91890c4c296e6b17151d9e65c0bde5e2
mark <mark@mark@kyin.org>
2019-02-25 12:22:21 -0500

    start project

---
+++ mei_ebds_drv/src/mei_ebds_drv.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : mei_ebds_drv.c
+ Author      : Mark Meadows
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("DHT11"); /* prints DHT11 */
+	return EXIT_SUCCESS;
+}
